639b5e09012dba1000bb138659f0b03883016b9f
yx-chan131 <yx.chan131@gmail.com>
2025-07-15 22:33:33 +0800

    update code structure and readme

--- README.md
+++ README.md
@@ -1,7 +1,48 @@
-# Sample curl commands
+# OCR Endpoint API
+
+## Overview
+An AI-powered FastAPI service that processes OCR text from documents and classifies the document type (e.g., medical certificate, referral letter, receipt) and extracts relevant structured fields.
+
+## Installation & Environment Setup (Windows)
+This application has been tested and verified to run successfully on the following system configuration:
+```yaml
+Operating System: Windows 11 Pro (64-bit)  
+Processor:        Intel(R) Core(TM) i5-14400F @ 2.50 GHz  
+Installed RAM:    32.0 GB 
+GPU:              NVIDIA GeForce RTX 4070 Super (12GB)
+```
+>⚠️ **Note**: While this app may run on lower-spec machines, performance and compatibility are only guaranteed on the above configuration. A dedicated GPU is not strictly required but can help with model inference and speed if applicable.
+
+#### ✅ Step 1: Make Sure Python 3.10 is Installed
+Check if Python 3.10 is already installed:
+```bash
+python --version
+```
+If not installed, download Python 3.10 from the official site: https://www.python.org/downloads/release/python-31011/
+
+#### ✅ Step 2: Create a Virtual Environment
+Use python3.10 to create a virtual environment named `venv`:
+```bash
+py -3.10 -m venv venv
+```
+
+#### ✅ Step 3: Activate the Environment
+On Windows:
+```bash
+venv\Scripts\activate
+```
+
+#### ✅ Step 4: Install Dependencies
+```bash
+pip install -r requirements.txt
+```
+
+
+
+
+## Sample curl commands
 ```bash
 
-curl -X POST "http://127.0.0.1:8000/ocr" -H "accept: application/json" -H "Content-Ty
-pe: multipart/form-data" -F "file=@data\medical_certificate.pdf"
+curl -X POST "http://127.0.0.1:8000/ocr" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@data\medical_certificate.pdf"
 
 ```
--- main.py
+++ main.py
@@ -63,53 +63,56 @@ async def upload_file(file: UploadFile = File(...)):
             detail="file_missing"
         )
     
-    img = read_data_to_image(contents, file.filename.endswith(".pdf"))
+    try: 
+        img = read_data_to_image(contents, file.filename.endswith(".pdf"))
 
-    result_dict = {}    
+        result_dict = {}    
 
-    if DEBUG_MODE:
-        save_name = f"{imname}.png"
-        save_path = os.path.join(".\debug", save_name)
-        cv2.imwrite(save_path, img[..., ::-1])
-    
-    # Call the OCR process function
-    out_str, _ = OCRModel.predict(img[..., ::-1], DEBUG_MODE, imname)
-    if not out_str:
-        raise HTTPException(
-            status_code=422, 
-            detail="unsupported_document_type"
-        )
-    
-    try:
+        if DEBUG_MODE:
+            save_name = f"{imname}.png"
+            save_path = os.path.join(".\debug", save_name)
+            cv2.imwrite(save_path, img[..., ::-1])
+        
+        # Call the OCR process function
+        out_str, _ = OCRModel.predict(img[..., ::-1], DEBUG_MODE, imname)
+        if not out_str:
+            raise HTTPException(
+                status_code=422, 
+                detail="unsupported_document_type"
+            )        
+        
         agent_out = agent.run(out_str)    
-    except:
-        raise HTTPException(
-            status_code=500, 
-            detail="internal_server_error"
-        )
+        
 
-    if "document_type" not in agent_out.lower() and "false" in agent_out.lower():
-        raise HTTPException(
-            status_code=422, 
-            detail="unsupported_document_type"
-        )
-    else:        
-        data = json.loads(agent_out)
+        if "document_type" not in agent_out.lower() and "false" in agent_out.lower():
+            raise HTTPException(
+                status_code=422, 
+                detail="unsupported_document_type"
+            )
+        else:        
+            data = json.loads(agent_out)
+        
+        document_type = data.pop("document_type")
+
+        if document_type == "referral_letter":
+            signPresence = SignDetect.predict(img[..., ::-1].copy(), DEBUG_MODE, imname)
+            # signPresence = SignDetect.predict(img[..., ::-1], DEBUG_MODE, imname)
+            data["signature_presence"] = signPresence
+
+        t2 = time.time()
+
+        result_dict["message"] = "Processing completed."
+        # result_dict["result_OCR"] = out_str
+        result_dict["result"] = {}
+        result_dict["result"]["document_type"] = document_type
+        result_dict["result"]["total_time"] = round(t2 - t1, 2)
+        result_dict["result"]["finalJson"] = data
+
+        content = json.dumps(result_dict, indent=2)    
+        return JSONResponse(content=json.loads(content))
     
-    document_type = data.pop("document_type")
-
-    if document_type == "referral_letter":
-       signPresence = SignDetect.predict(img[..., ::-1].copy(), DEBUG_MODE, imname)
-       data["signature_presence"] = signPresence
-
-    t2 = time.time()
-
-    result_dict["message"] = "Processing completed."
-    # result_dict["result_OCR"] = out_str
-    result_dict["result"] = {}
-    result_dict["result"]["document_type"] = document_type
-    result_dict["result"]["total_time"] = round(t2 - t1, 2)
-    result_dict["result"]["finalJson"] = data
-
-    content = json.dumps(result_dict, indent=2)    
-    return JSONResponse(content=json.loads(content))
+    except:
+            raise HTTPException(
+                status_code=500, 
+                detail="internal_server_error"
+            )
